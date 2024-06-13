ffd89532ee4bbee683bbbb88418e439bd18cea23
pisslin <gigi@gmail.com>
2024-06-13 21:49:39 +0200

    commit message

--- src/main/java/com/openclassrooms/testing/calcul/CalculatorApp.java
+++ src/main/java/com/openclassrooms/testing/calcul/CalculatorApp.java
@@ -7,6 +7,7 @@ import org.springframework.boot.autoconfigure.SpringBootApplication;
 public class CalculatorApp {
 
     public static void main(String[] args) {
+    	System.out.println("main");
         SpringApplication.run(CalculatorApp.class, args);
     }
 }
